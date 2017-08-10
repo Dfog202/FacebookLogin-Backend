c5f449aab516043084446d16f100a9b70dbdeefc
Seungha Lee <poopa8788@gmail.com>
2017-08-10 17:12:38 +0900

    User필드추가, create_facebook_user메서드 추가 및 FacebookLoginAPIView구현완료

--- django_app/member/apis.py
+++ django_app/member/apis.py
@@ -1,7 +1,10 @@
 import requests
+from rest_framework.authtoken.models import Token
 from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from member.models import User
 
 
 class FacebookLoginAPIView(APIView):
@@ -16,8 +19,29 @@ class FacebookLoginAPIView(APIView):
         token = request.data.get('token')
         if not token:
             raise APIException('token require')
-        debug_result = self.debug_token(token)
-        return Response(debug_result)
+
+        # 프론트로부터 전달받은 token을 Facebook의 debug_token API를 사용해
+        # 검증한 결과를 debug_result에 할당
+        self.debug_token(token)
+        user_info = self.get_user_info(token=token)
+        # 이미존재하면 가져오고 아니면 create_facebook_user메서드를 사용
+        if User.objects.filter(username=user_info['id']).exists():
+            user = User.objects.get(username=user_info['id'])
+        else:
+            user = User.objects.create_facebook_user(user_info)
+
+        # DRF token을 생성
+        token, token_created = Token.objects.get_or_create(user=user)
+
+        # 관련정보를 한번에 리턴
+        ret = {
+            'token': token.key,
+            'user': {
+                'pk': user.pk,
+                'username': user.username
+            }
+        }
+        return Response(ret)
 
     def debug_token(self, token):
         """
@@ -34,4 +58,21 @@ class FacebookLoginAPIView(APIView):
         result = response.json()
         if 'error' in result['data']:
             raise APIException('token invalid')
+        return result
+
+    def get_user_info(self, user_id, token):
+        url_user_info = 'https://graph.facebook.com/v2.9/me'
+        url_user_info_params = {
+            'access_token': token,
+            'fields': ','.join([
+                'id',
+                'name',
+                'first_name',
+                'last_name',
+                'picture.type(large)',
+                'gender',
+            ])
+        }
+        response = requests.get(url_user_info, params=url_user_info_params)
+        result = response.json()
         return result
